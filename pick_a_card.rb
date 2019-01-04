86a391c0ffab7daa8591b285a8c666a74282a7fc
Evgeniy A. Alabushev <alabushev-evgeniy@yandex.ru>
2019-01-04 14:18:30 +0500

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,16 @@
 #encoding: UTF-8
+
+# Этот код необходим только при использовании unicode-символов на Windows
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
 values = %w(2 3 4 5 6 7 8 J Q K A)
-suits = %w(Diamonds Hearts Clubs Spades) #Буби черви крести пики
+suits = %w(♦ ♥ ♣ ♠) #Буби черви крести пики
 
 puts "#{values.sample} of #{suits.sample}"
